b336153b1ab6d35702d45964709d8558bb9ffa02
Aishwarya Gudla <gudlaaishwarya@gmail.com>
2026-03-03 00:56:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from algosdk import account, mnemonic
 
+# Generate new Algorand account
+private_key, address = account.generate_account()
+
+print("Generated Algorand Account:")
+print("Address:", address)
+print("Private Key:", private_key)
+
+# Convert private key to mnemonic
+mnemo = mnemonic.from_private_key(private_key)
+print("Mnemonic:", mnemo)
